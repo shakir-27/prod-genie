b0d82eb1e1e528a8e755470c81ad3c0129273eb9
Shakir <shakir.ali@techolution.com>
2025-10-31 16:50:21 +0530

    new endpoint

--- backend/main.py
+++ backend/main.py
@@ -10,5 +10,10 @@ async def ping():
     return {"result": "pong"}
 
 
+@app.get("/echo/{message}")
+async def echo(message: str):
+    return {"message": message}
+
+
 if __name__ == "__main__":
     uvicorn.run(app="main:app", reload=True, workers=8)
